d12ed3743aab9c42a9b6bc41a01eb1cc130ac390
Andreas Herz <a.herz@freegroup.de>
2016-09-23 18:14:17 +0200

    automate publish to nodeMCU

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var shell = require('shelljs');
+
 module.exports = function (grunt) {
 
     //Initializing the configuration object
@@ -14,18 +16,31 @@ module.exports = function (grunt) {
                 src: ['*'],
                 dest: 'dist'
             }
-
         }
+
+
     });
 
     // Plugin loading
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('upload', 'Generates JSON file with all shape files', function() {
+        // List all SHAPE files in the templates directory.
+        //
+        var list = [];
+        var shapes = grunt.file.expand({filter: "isFile", cwd: "./dist/"},["**/*"]);
+        shapes.forEach(function(shape){
+       //     console.log(shape);
+            shell.exec('nodemcu-tool -b 115200 -p /dev/tty.wchusbserial640 upload ./dist/'+shape);
+        });
+
+    });
 
     // Task definition
     grunt.registerTask('default', [
         'clean',
-        'copy'
+        'copy',
+        'upload'
     ]);
 };
 
